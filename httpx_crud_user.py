ec08643912e6d0d797b43f42ed9f6a6078b267e0
Konstantin <maschkovc@yandex.ru>
2025-12-07 18:32:59 +0300

    user crud

--- httpx_crud_user.py
+++ httpx_crud_user.py
@@ -9,7 +9,6 @@ user_payload = {
   "firstName": "Semen",
   "middleName": "Egor"
 }
-
 response = httpx.post('http://localhost:8000/api/v1/users', json=user_payload)
 user_payload_dara = response.json()
 print(f'Status: {response.status_code}, Response: {user_payload_dara}')
@@ -26,7 +25,31 @@ print(f'Status: {login_response.status_code}, Login response: {login_response_da
 # Получение пользователя
 user_headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
 user_id = user_payload_dara['user']['id']
-print(user_id)
+# print(user_id)
 response = httpx.get(f'http://localhost:8000/api/v1/users/{user_id}', headers=user_headers)
 user_id_data = response.json()
 print(f'Status: {response.status_code}, Response: {user_id_data}')
+
+
+patch_user_payload = {
+  "email": get_random_email(),
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+response = httpx.patch(f'http://localhost:8000/api/v1/users/{user_id}', json=patch_user_payload, headers=user_headers)
+patch_user_data = response.json()
+print(f'Status: {response.status_code}, Response: {patch_user_data}')
+
+# удаление пользователя
+response = httpx.delete(f'http://localhost:8000/api/v1/users/{user_id}', headers=user_headers)
+delete_respons_data = response.json()
+print(f'Status: {response.status_code}, Response: {delete_respons_data}')
+
+# Получение пользователя
+# user_headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+# user_id = user_payload_dara['user']['id']
+# # print(user_id)
+# response = httpx.get(f'http://localhost:8000/api/v1/users/{user_id}', headers=user_headers)
+# user_id_data = response.json()
+# print(f'Status: {response.status_code}, Response: {user_id_data}')
