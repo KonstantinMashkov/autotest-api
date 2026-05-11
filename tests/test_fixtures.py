4dfd9d65be9d64a0d1e2ae92c1839e9b16522172
Konstantin <maschkovc@yandex.ru>
2026-05-11 14:28:43 +0300

    fixtures

---
+++ tests/test_fixtures.py
@@ -0,0 +1,13 @@
+
+
+class TestUserFlow:
+    def test_user_can_login(self, settings, user, users_client):
+        pass
+
+    def test_user_can_create_course(self, settings, user, users_client):
+        pass
+
+
+class TestAccountFlow:
+    def test_user_account(self, settings, user, users_client):
+        pass
